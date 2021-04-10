6c41d17846cea99ace3c29d1614782fa76bb6b73
Aliraza10199 <aliraza10199@gmail.com>
2021-04-10 21:34:49 +0500

    first comment

---
+++ mysqli_connect.php
@@ -0,0 +1,18 @@
+<?php
+
+
+class mysqli_connect
+{
+
+    /**
+     * mysqli_connect constructor.
+     * @param mixed $fname
+     * @param mixed $Lname
+     * @param mixed $email
+     * @param string $password
+     * @param mixed $Pnumber
+     */
+    public function __construct($fname, $Lname, $email, string $password, $Pnumber)
+    {
+    }
+}
